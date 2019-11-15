d88266ed027112714ca6c04410e30aa561d59299
qlbmt <331930347@qq.com>
2019-11-15 09:51:50 +0800

    github登录，将参数修改到properties。

--- src/main/java/life/majiang/luntan/controller/AuthorizeController.java
+++ src/main/java/life/majiang/luntan/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.majiang.luntan.dto.AccessTokenDTO;
 import life.majiang.luntan.dto.GithubUser;
 import life.majiang.luntan.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +20,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect_uri}")
+    private String redirect_uri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state , Model model) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("666cd028de094aca1efa");
-        accessTokenDTO.setClient_secret("a097951bb5d2a0b6059501e9acaa4c4674d1226e");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
 
         String accessToken =  githubProvider.getAccessToken(accessTokenDTO);
