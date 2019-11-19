115b2da69bd550f30132e20674edfbb14fead864
qlbmt <331930347@qq.com>
2019-11-19 15:17:57 +0800

    连接mysql写入用户数据

---
+++ src/main/java/life/majiang/luntan/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package life.majiang.luntan.mapper;
+
+/**
+ * Created by admin on 2019/11/18.
+ */
+public class UserMapper {
+}
---
+++ src/main/java/life/majiang/luntan/model/User.java
@@ -0,0 +1,7 @@
+package life.majiang.luntan.model;
+
+/**
+ * Created by admin on 2019/11/18.
+ */
+public class User {
+}
